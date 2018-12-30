4cf85cb6c6227edb6ba36dcced9af4851cd68ee5
sekab <Ataher@entrepreware.com>
2018-12-30 13:20:56 +0200

    initiated angular application to be the front-end

--- server/app.py
+++ server/app.py
@@ -1,31 +1,8 @@
 from flask import Flask, render_template 
 from flask_socketio import SocketIO , send , Namespace ,join_room, leave_room
 from users import *
+from threading import Timer
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
-
-Rooms = list()
-UserList =  list()
-TalkingQueue =  list()
-RoomList = list()
-Talker = ''
-
-if __name__ == '__main__':
-    # socketio.run(app)
-    # socketio.run()/
-    # socketio.run(app , host='0.0.0.0')
-    app.run(host='0.0.0.0' , port=4200 , debug=True)
-    # bottle = BottleControl('Bottle Room A')
-
-
-def changeTaler(newTalker):
-    if("Talker"):
-        Talker = newTalker
-    
-    return Talker
-    
 
 
 # join room  without being in the talking queue yet. 
@@ -35,54 +12,112 @@ def changeTaler(newTalker):
 
 
 
-# join functions
-
-@socketio.on('new user')
-def handle_new_user(data):
-    socketio.emit('new user' , data['name'])
 
 
-@socketio.on('request to talk')
-def handle_request_to_talk(data):
-    print('it came here')
-    User = {}    
-    TalkingQueue.append(data)
-    User['Talker'] = Talker
-    User['TalkingQueue'] = TalkingQueue
-    socketio.emit('updates' , User)
+# necessary startup code 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'no-wifi-1234' #idk 
+socketio = SocketIO(app)
+# declared lists to be used as long as the server is up.
+Rooms = dict()
 
 
-
+if __name__ == '__main__':
+    socketio.run(app)    
 
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
     send(message , broadcast=True)
     
+@socketio.on('connect')
+def test():
+    print('connected from client')
 
-
-
-
-    
-
-@socketio.on("create_room")
+@socketio.on("CreateRoom")
 def create_room(data):
-    roomTemp = Room(data["room"])
+    newRoom = Room(data["room"])
+    newUser = User(data["username"])
+    Rooms[newRoom.name] = newRoom
+    #addUser assigns a new id for the user and maps it to the name and returns the assigned id back to the user to use
+    AssignedID = newRoom.addUser(newUser)
+    print("created room with name :",newRoom)
+    join_room(newRoom.name)
+    print("{0} joined room".format(newUser.name))
+    send(newUser.name + ' has entered the room.', room=newRoom.name)
+    return AssignedID
+
+@socketio.on("JoinRoom")
+def add_user_to_room(data):
+    choosenRoom = Rooms[data["room"]]
+    userObject = User(data["username"])
+    AssignedID = choosenRoom.addUser(userObject)
+    join_room(data["room"])
+    print("{0} has joined the room".format(data["username"]))
+    send(data["username"] + ' has entered the room.',room= data["room"])
+    socket
+    return AssignedID
+
+@socketio.on("LeaveRoom")
+def remove_user_from_room(data):
+    leave_room(data["room"])
+    send(data["username"] + ' has left the room.',room= data["room"])
+
+def nextUser(roomName):
+    room=Rooms[roomName]
+    room.talker=''
+    if len(room.queue)>0:
+        UserToTalk = room.queue[0].name
+        userToTalkID = room.queue[0].id
+        del room.queue[0]
+        room.talker = UserToTalk
+        print("{0} started talking for 10 secs".format(room.talker))
+        queueTimer = Timer(10 , nextUser,roomName)
+        queueTimer.start()
+        room.talkerThread = queueTimer
+    else:
+        return
+
+@socketio.on("JoinQueue")
+def JoinQueue(data):
+    roomName = data["room"]
+    room=Rooms[roomName]
     
-    userTemp = User(data["username"] , "asadas")
-    roomTemp.addUser(userTemp)
+    #Whenever there is no one in
+    if len(room.queue)==0 and room.talker =='':
+        room.talker = data["username"]
+        queueTimer = Timer(10 , nextUser,roomName)
+        print("{0} started talking for 10 secs".format(room.talker))
+        queueTimer.start()
+        room.talkerThread = queueTimer
+    else:
+        userObject = User(data["username"])
+        userObject.setID(data["userID"])
+        room.queue.append(userObject)
+        print("{0} is added to queue".format(data["username"]))
 
-    Rooms.append(roomTemp)
 
-    print(Rooms[0].name)
+@socketio.on("leaveQueue")
+def leaveQueue(data):
+    userObject = User(data["username"])
+    userObject.setID(data["userid"])
 
-    #sending back to the users that x joined the room ffs!
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
 
-@app.route('/')
-def hello_world():
-   print("amora")
-   return 'helllo'
+@socketio.on("StopTalking")
+def StopTalking(data):
+    roomName = data["room"]
+    choosenRoom = Rooms[roomName]
+    choosenRoom.talkerThread.cancel()
+    nextUser(roomName)
+
+
+# @socketio.on("LeaveQueue")
+# def JoinQueue(data):
+
+# @socketio.on("StopTaking")
+# def StopTaking(data)
+
+# @app.route("return_avaialble_rooms")
+# def return_available_rooms():
+#     return Rooms
+        
--- server/users.py
+++ server/users.py
@@ -1,18 +1,28 @@
 
 class Room:
-    setTime = 1
+
     def __init__(self, name ):
         self.name = name
         self.queue = list()
+        self.talker = ''
+        self.talkerThread = ''
+        self.userID_map_Username = dict()
+        self.Last_ID_value=0
+
     
     def addUser(self , user):
-        self.queue.append(user)
-
+        self.userID_map_Username[self.Last_ID_value] = user.name
+        user.setID(self.Last_ID_value)
+        self.Last_ID_value +=1
+        #the user's ID
+        return self.Last_ID_value -1
   
 
 class User:
-    def __init__(self , name , id):
+    def __init__(self , name ):
         self.name = name
-        self.id = id
+        self.id = ''
 
+    def setID(self,id):
+        self.id = id
 
